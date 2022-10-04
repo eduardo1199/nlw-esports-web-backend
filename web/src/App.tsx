2230b95e8f488b7f608b7b5ba65ba10684a07f27
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2022-10-04 19:20:31 -0300

    created modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,8 +9,7 @@ import Logo from './assets/logo.svg';
 import { GameBanner } from './components/GameBanner';
 import { CreatedAdBanner } from './components/CreatedAdBanner';
 import { GameController } from 'phosphor-react';
-
-
+import { Input } from './components/Form/Input';
 
 type Game = {
   bannerUrl: string;
@@ -65,56 +64,75 @@ function App() {
           <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-ld w-[480px] shadow-lg shadow-black-1/4">
             <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
 
-            <Dialog.Content className="">
-              <form>
-                <div>
-                  <label htmlFor="game">Qual o game?</label>
-                  <input placeholder="Selecione o game que deseja jogar" id="game" />
+              <form className="mt-8 flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <label htmlFor="game" className="font-semibold">Qual o game?</label>
+                  <Input
+                    placeholder="Selecione o game que deseja jogar"
+                    id="game" 
+                  />
                 </div>
 
-                <div>
+                <div className="flex flex-col gap-2">
                   <label htmlFor="name">Seu nome (ou nickname)</label>
-                  <input id="name" placeholder="Como te chamam dentro do game?" />
+                  <Input id="name" placeholder="Como te chamam dentro do game?" />
                 </div>
 
-                <div>
-                  <div>
+                <div className="grid grid-cols-2 gap-6">
+                  <div className="flex flex-col gap-2">
                     <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                    <input type="number" id="yearsPlaying" placeholder="Tudo bem ser ZERO"/>
+                    <Input type="number" id="yearsPlaying" placeholder="Tudo bem ser ZERO"/>
                   </div>
-                  <div>
+                  <div className="flex flex-col gap-2">
                     <label htmlFor="discord">Joga há quantos anos?</label>
-                    <input type="text" id="discord" placeholder="Usuario#0000"/>
+                    <Input type="text" id="discord" placeholder="Usuario#0000"/>
                   </div>
                 </div>
 
-                <div>
-                  <div>
+                <div className="flex gap-6">
+                  <div className="flex flex-col gap-2">
                     <label htmlFor="weekDays">Quando costuma jogar?</label>
+                    
+                    <div className="grid grid-cols-4 gap-2">
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Domingo">D</button>
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Segunda">S</button>
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Terça">T</button>
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Quarta">Q</button>
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Quinta">Q</button>
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Sexta">S</button>
+                      <button className="w-8 h-8 rounded bg-zinc-900"title="Sábado">S</button>
+                    </div>
                   </div>
-                  <div>
+                  <div className="flex flex-col gap-2 flex-1">
                     <label htmlFor="hourStart">Qual horário do dia?</label>
-                    <div>
-                      <input id="hourStart" type="time" placeholder="De" />
-                      <input id="hourEnd" type="time" placeholder="De" />
+                    <div className="grid grid-cols-2 gap-2">
+                      <Input id="hourStart" type="time" placeholder="De" />
+                      <Input id="hourEnd" type="time" placeholder="De" />
                     </div>
                   </div>
                 </div>
 
-                <div>
-                  <input type="checkbox" />
+                <div className="mt-2 flex gap-2 text-small">
+                  <Input type="checkbox" />
                   Costumo me conectar ao chat de voz
                 </div>
 
-                <footer>
-                  <button type="button">Cancelar</button>
-                  <button type="submit">
-                    <GameController />
+                <footer className="mt-4 flex justify-end gap-4">
+                  <Dialog.Close
+                    type="button"
+                    className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600"
+                  >
+                      Cancelar
+                  </Dialog.Close>
+                  <button
+                    type="submit"
+                    className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+                  >
+                    <GameController className="w-6 h-6" />
                     Encontrar duo
                   </button>
                 </footer>
               </form>
-            </Dialog.Content>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
